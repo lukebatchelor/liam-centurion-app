e4692b4a2a05746064da42c5edf0cef29ce9e57e
Luke.Batchelor <batchelor.luke+git@gmail.com>
2023-02-23 18:49:43 +0800

    Code cleanup

--- src/components/PlayingScreen.tsx
+++ src/components/PlayingScreen.tsx
@@ -9,38 +9,28 @@ const debugJumpTimeSecs = params.has("debug") ? Number(params.get("debug")) : 0;
 
 type Props = {};
 export default function PlayingScreen(props: Props) {
-  const [count, setCount] = useState(0);
   const [drinkCount, setDrinkCount] = useState(0);
   const [startTime, setStartTime] = useState<number | null>(null);
+  const [totalSecondsSinceStart, setTotalSecondsSinceStart] = useState(0);
 
   useEffect(() => setStartTime(Date.now()), []);
-  const update = () => {
-    setCount(count + 1);
-  };
-  useInterval(update, 500);
+  useInterval(() => setTotalSecondsSinceStart(Math.floor((Date.now() - Number(startTime)) / 1000)), 500);
 
   if (!startTime) return null;
-  const totalSecondsSinceStart = Math.floor((Date.now() - startTime) / 1000);
   const minsSinceStart = Math.floor(totalSecondsSinceStart / 60);
   const secondsSinceMinStart = totalSecondsSinceStart - minsSinceStart * 60;
-  const timerStr = `${padNumber(minsSinceStart)}:${padNumber(
-    secondsSinceMinStart
-  )}`;
+  const timerStr = `${padNumber(minsSinceStart)}:${padNumber(secondsSinceMinStart)}`;
   const drinksOwed = minsSinceStart + 1;
   const waiting = drinksOwed <= drinkCount;
   const gameOver = drinkCount === 100;
-  const drinkButtonText = gameOver
-    ? "YEWWWWW"
-    : waiting
-    ? "Waiting..."
-    : "Drink!";
+  const drinkButtonText = gameOver ? "YEWWWWW" : waiting ? "Waiting..." : "Drink!";
   const drinkButtonAnimation = waiting || gameOver ? "" : "animate-custom-ping";
+  const btnClasses = `${drinkButtonAnimation} text-white bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-xl px-10 py-4 text-center mr-2 mb-2`;
+
   const debugAddTime = () => {
     setStartTime((startTime) => Number(startTime) - debugJumpTimeSecs * 1000);
   };
-  const onDrinkClick = () => {
-    setDrinkCount(drinkCount + 1);
-  };
+  const onDrinkClick = () => setDrinkCount(drinkCount + 1);
 
   return (
     <div className="p-8">
@@ -55,34 +45,17 @@ export default function PlayingScreen(props: Props) {
               {Array.from({ length: 10 }).map((_el, colIdx) => {
                 const drinkIdx = rowIdx * 10 + colIdx;
                 const grayscale = drinkIdx >= drinkCount ? "grayscale" : "";
-                const contrast =
-                  drinkIdx >= drinkCount && drinkIdx >= drinksOwed
-                    ? "contrast-50"
-                    : "";
+                const contrast = drinkIdx >= drinkCount && drinkIdx >= drinksOwed ? "contrast-50" : "";
 
-                const dropshadow =
-                  drinkIdx === drinkCount
-                    ? "drop-shadow-[2px_4px_6px_black]"
-                    : "";
-                return (
-                  <img
-                    src={shotGlassSvg}
-                    alt="shot glass"
-                    className={`h-8 m-1 ${grayscale} ${dropshadow} ${contrast}`}
-                  />
-                );
+                const dropshadow = drinkIdx === drinkCount ? "drop-shadow-[2px_4px_6px_black]" : "";
+                return <img src={shotGlassSvg} alt="shot glass" className={`h-8 m-1 ${grayscale} ${dropshadow} ${contrast}`} />;
               })}
             </div>
           );
         })}
       </div>
       <div className="flex justify-center pt-8">
-        <button
-          type="button"
-          disabled={waiting || gameOver}
-          onClick={onDrinkClick}
-          className={`text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-xl px-10 py-4 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 ${drinkButtonAnimation}`}
-        >
+        <button type="button" disabled={waiting || gameOver} onClick={onDrinkClick} className={btnClasses}>
           {drinkButtonText}
         </button>
       </div>
